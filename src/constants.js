cb99b2553493acc32c6dfe97019951fa06769692
Christoffer Bro Sorensen <christoffer.sorensen@accenture.com>
2022-04-13 12:27:46 +0200

    Added createGrid() function

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const NUM_COLUMNS = 25;
+export const NUM_ROWS = 25;
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,17 +1,66 @@
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import React from "react";
+import { SafeAreaView, Text, StyleSheet, FlatList, View } from "react-native";
+import { NUM_COLUMNS, NUM_ROWS } from "../constants";
 
-const GameScreen = (navigation) => (
-  <SafeAreaView style={styles.container}>
-    <Text>Game goes here!</Text>
-  </SafeAreaView>
+const createGrid = () => {
+  let grid = new Array(NUM_COLUMNS * NUM_ROWS);
+
+  for (let xx = 0; xx < NUM_COLUMNS; xx++) {
+    for (let yy = 0; yy < NUM_ROWS; yy++) {
+      grid[NUM_COLUMNS * yy + xx] = { hasSnake: false, id: `(${xx},${yy})` };
+    }
+  }
+
+  const middleIdx = Math.round(
+    NUM_COLUMNS * Math.round(NUM_ROWS / 2) + Math.round(NUM_COLUMNS / 2)
+  );
+
+  //DEBUG
+  grid[middleIdx].hasSnake = 1;
+  grid[middleIdx - 1].hasSnake = 1;
+  grid[middleIdx + 1].hasSnake = 1;
+
+  return grid;
+};
+
+const renderBox = ({ item }) => (
+  <View style={[styles.item, item.hasSnake && styles.snakeBody]}></View>
 );
+
+const GameScreen = (navigation) => {
+  const data = createGrid();
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <Text>Game goes here!</Text> */}
+      <FlatList
+        style={styles.container}
+        data={data}
+        renderItem={renderBox}
+        numColumns={NUM_COLUMNS}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignSelf: "center",
+    backgroundColor: "aliceblue",
+    // justifyContent: "center",
+    // alignSelf: "center",
+    flexDirection: "column",
+    alignContent: "center",
   },
+  item: {
+    backgroundColor: "#aaa",
+    padding: 6,
+    flex: 1,
+    alignSelf: "center",
+    // marginVertical: 0.5,
+    // marginHorizontal: 0.5,
+  },
+  snakeBody: { backgroundColor: "#fed" },
 });
 
 export default GameScreen;

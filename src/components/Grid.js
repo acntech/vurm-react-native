1f8b664f069a079f3ee22e96d3e058b65acd683b
Christoffer Bro Sorensen <christoffer.sorensen@accenture.com>
2022-05-13 13:27:37 +0200

    Revamp of grid, now reuses previous grid and runs (update) with useEffect hook instead of recreation each re-render

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,42 +2,60 @@ import { NUM_COLUMNS, NUM_ROWS } from "../constants";
 import { FlatList, StyleSheet, View, Dimensions } from "react-native";
 import { Component } from "react";
 import { coordToIdx } from "../utilities/conversion";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+import deepcopy from "deepcopy";
 
-export default Grid = ({ snakeCoords, berryCoord }) => (
-  <FlatList
-    style={styles.container}
-    data={createGridData(snakeCoords, berryCoord)}
-    renderItem={renderBox}
-    numColumns={NUM_COLUMNS}
-    keyExtractor={keyExtractor}
-    scrollEnabled={false}
-  />
-);
+export default Grid = ({ snakeCoords, berryCoord }) => {
+  const [gridData, setGridData] = useState(createInitializedGridData());
+  const prevSnakeCoords = useRef([]);
+  const prevBerryCoord = useRef(null);
+
+  const updateData = () => {
+    newData = deepcopy(gridData);
+    prevSnakeCoords.current.forEach((coord) => {
+      newData[coordToIdx(coord)].hasSnake = false;
+    });
+
+    if (prevBerryCoord.current != null) {
+      newData[coordToIdx(prevBerryCoord.current)].hasBerry = false;
+    }
+
+    if (prevSnakeCoords.current.length > 0) {
+      newData[coordToIdx(prevSnakeCoords.current[0])].hasSnakeHead = false;
+    }
+
+    snakeCoords.forEach((coord) => {
+      newData[coordToIdx(coord)].hasSnake = true;
+    });
+
+    if (berryCoord != null) {
+      newData[coordToIdx(berryCoord)].hasBerry = true;
+    }
+    newData[coordToIdx(snakeCoords[0])].hasSnakeHead = true;
+
+    setGridData(newData);
+    prevSnakeCoords.current = snakeCoords;
+    prevBerryCoord.current = berryCoord;
+  };
+
+  useEffect(() => {
+    updateData();
+  }, [snakeCoords, berryCoord]);
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={gridData}
+      renderItem={renderBox}
+      numColumns={NUM_COLUMNS}
+      keyExtractor={keyExtractor}
+      scrollEnabled={false}
+    />
+  );
+};
 
 const keyExtractor = (item) => item.id;
-
-const createGridData = (snakeCoords, berryCoord) => {
-  let gridData = new Array(NUM_COLUMNS * NUM_ROWS);
-
-  for (let i = 0; i < gridData.length; i++) {
-    gridData[i] = {
-      hasSnake: false,
-      hasSnakeHead: false,
-      hasBerry: false,
-      id: i,
-    };
-  }
-
-  snakeCoords.forEach((coord) => {
-    gridData[coordToIdx(coord)].hasSnake = true;
-  });
-
-  if (berryCoord != null) {
-    gridData[coordToIdx(berryCoord)].hasBerry = true;
-  }
-  gridData[coordToIdx(snakeCoords[0])].hasSnakeHead = true;
-  return gridData;
-};
 
 const renderBox = ({ item }) => {
   const height = Dimensions.get("window").height;
@@ -68,6 +86,20 @@ class Box extends Component {
     return <View style={this.props.style}></View>;
   }
 }
+
+const createInitializedGridData = () => {
+  let data = new Array(NUM_COLUMNS * NUM_ROWS);
+
+  for (let i = 0; i < data.length; i++) {
+    data[i] = {
+      hasSnake: false,
+      hasSnakeHead: false,
+      hasBerry: false,
+      id: i,
+    };
+  }
+  return data;
+};
 
 const styles = StyleSheet.create({
   container: {
